a2e07a1968467c69eece5ebbc51a6e93e7ad3702
Sivadharshini99 <dharshu0010@gmail.com>
2025-03-18 23:16:09 +0530

    modified  files

---
+++ JAVA/springbootproject/src/main/java/com/exampleprgm/springbootproject/calculateSum.java
@@ -0,0 +1,16 @@
+package com.exampleprgm.springbootproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class calculateSum {
+
+    @GetMapping("/calculation")
+    public String calculation(){
+        int x = 5; 
+        int y = 6; 
+        int sum = x + y; 
+        return "The sum of " + x + " and " + y + " is: " + sum;
+    }
+}
